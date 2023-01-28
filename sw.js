d7109fc71ef1db25cb4cc2e97657446e8e0863cb
bernhard759 <bernhard.gailer@gmx.de>
2023-01-28 13:54:09 +0100

    some changes

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
 // SW Install
-self.addEventListener("install", e => {
-    // wait until resources are cached
-    e.waitUntil(
-        caches.open("static").then( cache => {
-            return cache.addAll(["./", "./src/css/style.css", "./src/js/script.js", "./img"]);
-        })
-    )
+self.addEventListener("install", (e) => {
+  // wait until resources are cached
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll([
+        "./",
+      ]);
+    })
+  );
 });
 
 // SW Fetch
-self.addEventListener("fetch", e => {
-    console.log(`fetch request for ${e.request.url}`);
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            // get resources from cache first, if not there then go to the network
-            return response || fetch(e.request);
-        })
-    )
-})
+self.addEventListener("fetch", (e) => {
+  console.log(`fetch request for ${e.request.url}`);
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      // get resources from cache first, if not there then go to the network
+      return response || fetch(e.request);
+    })
+  );
+});
